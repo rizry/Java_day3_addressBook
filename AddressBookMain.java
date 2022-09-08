72dba50d9872144ff46fd3b8e1936559aea85eb9
rizry <rizzwan.ansarii@gmail.com>
2022-09-08 16:06:45 +0530

    created a contact in AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,7 +3,13 @@ package com.java.day9;
 public class AddressBookMain {
 
   public static void main(String[] args) {
-    System.out.println("--Welcome to Address Book Program--");
-  }
+  System.out.println("--Welcome to Address Book Program--");
 
+  Contact c = new Contact();
+  c.getInputs();
+
+  System.out.println("\nHere's what was entered: " + c.fName + " " + c.lName + " " + c.address + " " + c.city + " "
+    + c.state + " " + c.email + " " + c.zip + " " + c.phNum);
+
+  }
 }
---
+++ Contact.java
@@ -0,0 +1,38 @@
+package com.java.day9;
+
+import java.util.Scanner;
+
+public class Contact {
+  String fName, lName, address, city, state, email;
+  int zip;
+  long phNum;
+
+  Scanner sc = new Scanner(System.in);
+
+  void getInputs() {
+    System.out.print("Enter your first name: ");
+    fName = sc.nextLine();
+
+    System.out.print("Enter your last name: ");
+    lName = sc.nextLine();
+
+    System.out.print("Enter your address: ");
+    address = sc.nextLine();
+
+    System.out.print("Enter your city: ");
+    city = sc.nextLine();
+
+    System.out.print("Enter your state: ");
+    state = sc.nextLine();
+
+    System.out.print("Enter your email: ");
+    email = sc.nextLine();
+
+    System.out.print("Enter your area zip code: ");
+    zip = sc.nextInt();
+
+    System.out.print("Enter your phone number: ");
+    phNum = sc.nextLong();
+
+  }
+}
